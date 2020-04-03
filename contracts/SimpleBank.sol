ce513826e8402486b2db36e886cf0122e91324a7
vibhorg <vibhorg7@gmail.com>
2020-04-04 01:20:18 +0530

    initial commit

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -2,59 +2,62 @@ pragma solidity ^0.5.8;
 
 contract SimpleBank {
     uint8 private clientCount;
-    mapping (address => uint) private balances;
+    mapping (address => uint)  balances;
     address public owner;
 
-  // Log the event about a deposit being made by an address and its amount
-    event LogDepositMade(address indexed accountAddress, uint amount);
 
-    // Constructor is "payable" so it can receive the initial funding of 30, 
-    // required to reward the first 3 clients
-    constructor() public payable {
-        require(msg.value == 30 ether, "30 ether initial funding required");
-        /* Set the owner to the creator of this contract */
+
+
+    constructor() public{
         owner = msg.sender;
-        clientCount = 0;
+        balances[msg.sender]=0;
     }
 
-    /// @notice Enroll a customer with the bank, 
-    /// giving the first 3 of them 10 ether as reward
-    /// @return The balance of the user after enrolling
+
     function enroll() public returns (uint) {
-        if (clientCount < 3) {
-            clientCount++;
-            balances[msg.sender] = 10 ether;
-        }
+        uint val=10;
+            balances[msg.sender] = val;
+        return balances[msg.sender];
+    }
+    function newenroll(address acc) private {
+        balances[acc]+=0;
+    }
+
+
+    function deposit(uint depositAmount) public returns (uint) {
+        balances[msg.sender] += depositAmount;
         return balances[msg.sender];
     }
 
-    /// @notice Deposit ether into bank, requires method is "payable"
-    /// @return The balance of the user after the deposit is made
-    function deposit() public payable returns (uint) {
-        balances[msg.sender] += msg.value;
-        emit LogDepositMade(msg.sender, msg.value);
-        return balances[msg.sender];
-    }
 
-    /// @notice Withdraw ether from bank
-    /// @return The balance remaining for the user
-    function withdraw(uint withdrawAmount) public returns (uint remainingBal) {
+    function withdraw(uint withdrawAmount) public returns (uint) {
         // Check enough balance available, otherwise just return balance
         if (withdrawAmount <= balances[msg.sender]) {
             balances[msg.sender] -= withdrawAmount;
-            msg.sender.transfer(withdrawAmount);
         }
         return balances[msg.sender];
     }
+    function transfer(address to,uint amount) public returns (uint){
+        newenroll(to);
+        if(amount<=balances[msg.sender]){
+            balances[msg.sender]-=amount;
+            balances[to]+=amount;
+            
+            
+        }
+        return balances[to];
+        
+    }
 
-    /// @notice Just reads balance of the account requesting, so "constant"
-    /// @return The balance of the user
+
     function balance() public view returns (uint) {
         return balances[msg.sender];
     }
-
-    /// @return The balance of the Simple Bank contract
-    function depositsBalance() public view returns (uint) {
-        return address(this).balance;
+    function checkbal(address ch) public view returns(uint){
+        return balances[ch];
     }
+
+
 }
+
+
